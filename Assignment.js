78be2c878d0123038b55cf467f6b19dae6a2fa84
Zawad Bin Zahed <zawadtahmid@gmail.com>
2021-01-22 11:46:18 +0800

    testing

--- Assignment.js
+++ Assignment.js
@@ -4,18 +4,15 @@
 // 1 killometer = 1000 meter
     // var kilometer = 1;
     // var meter = 1000;
-// function kilometerTometer (kilometer){
-    
-//     var meter = kilometer/1000;
-//     return meter; 
+
+
+
+// function inchToFeet(inch){
+//     var feet = inch/12;
+//     return feet;
 // }
-// // var hightJump(50);
-// console.log(kilometer);
+// var nanaFeet = inchToFeet(156);
+// console.log(feet);
 
-
-function inchToFeet(inch){
-    var feet = inch/12;
-    return feet;
-}
-var nanaFeet = inchToFeet(156);
+var inch = 156;
 console.log(inch);
