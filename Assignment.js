6a03fdebb371029d1e3a338e31b9ef1e0a7d1e23
Zawad Bin Zahed <zawadtahmid@gmail.com>
2021-01-23 01:22:58 +0800

    update all

--- Assignment.js
+++ Assignment.js
@@ -2,64 +2,36 @@
 // https://github.com/Zawad-Zahed/Starting-JS
 
 
-
+// 1st problem Solved = "KilometerToMeter" 
 // 1 killometer = 1000 meter
 // kilometer = 1*1000 = meter 
 
-
+var kilometer = 1;
+var meter = 1000;
 function KilometerToMeter(kilometer){
     var meter = kilometer*1000;
     return meter;
 
 }
-var jeteHobe = KilometerToMeter(55);
-console.log(jeteHobe);
+var jeteHobe = KilometerToMeter(58);
+console.log(jeteHobe);  //Ans: 5800 meter.
 
 
-
-
-// 2nd problem Solve = "budgetCalculator" 
-/**function budgetCalculator( 
-    countWatch = 0 ,
-    countMobile = 0 , 
-    countLeptop =0
-    )
-    {
-        var costPerWatch = 50;
-        var costPerMobile = 100;
-        var costPerLeptop = 500;
-        var totaleCost = 0;
-        if (
-            typeof countWatch = 0;
-            typeof countMobile = 0; 
-            typeof countLeptop =0;
-        ){
-            else if (
-                countWatch < 0; countMobile < 0; countLeptop < 0;
-            ){
-                else 
-                return ( totaleCost = "countWatch" , "countMobile" , "countLeptop" )
-            }
-        }
-      
-}
-var totaleCost = "return";
-console.log(totaleCost); */
-
+// 2nd problem Solved = "budgetCalculator" 
 var costPerWatch = 50;
 var costPerMobile = 100;
 var costPerLeptop = 500;
-function budgetCalculator(countWatch = 0 ,
-    countMobile = 0 , 
-    countLeptop =0){
-        var totalWatch = costPerWatch * 50;
-        var totalMobile = costPerMobile * 100;
-        var totalLeptop = costPerLeptop * 500;
-        return totalWatch + totalMobile + totalLeptop;
-    }
-var result = budgetCalculator(2,1,1);
-console.log(result);
-
+    function budgetCalculator(countWatch,
+        countMobile, 
+        countLeptop){
+            var totalWatch = countWatch * 50;
+            var totalMobile = countMobile * 100;
+            var totalLeptop = countLeptop * 500;
+            return totalWatch + totalMobile + totalLeptop;
+        }
+    var result = budgetCalculator(5,4,3);
+    console.log(result); // Ans: budget lagbe 2150taka.
+    
 
 
 
@@ -71,7 +43,7 @@ function hotelCost(roomCost){
     }
     else if( roomCost <= 20){
         var firstSchedule = 10 * 100;
-        var inSchedule = roomCost -10;
+        var inSchedule = roomCost - 10;
         var sceondSchedule = inSchedule * 80;
         roomCost = firstSchedule + sceondSchedule;
     }
@@ -84,24 +56,27 @@ function hotelCost(roomCost){
     }
     return roomCost;
 }
+
 var totalDayCount = hotelCost(17);
-console.log(totalDayCount);
+console.log(totalDayCount); //Ans: 17 diner jonne 1560Taka.
+
 
 // 4th problem Solve = "megaFriend" 
-var nameOfFriends = ["Sakauwa","Hashu","Mashu","Suki"];
+
+var nameOfFriends = ["Sakauwa","Hashu","Shukhaimura","Suki","Mashu"];
 var maxWord = nameOfFriends [0];
 
-function megaFriend (){
+function megaFriend (nameOfFriends){
     for ( var i = 0; i < nameOfFriends.length; i++){
         var element = nameOfFriends [i];
-        if ( element > maxWord.length){
+        if ( element.length > maxWord.length){
             maxWord = element;
         }
-        // else {
-        //     return ("small name")
-        // }
     }
+    
     return maxWord;
+    
 }
-var bigName = megaFriend(nameOfFriends);
-console.log(bigName);
+
+var bigNameOfFriends = megaFriend(nameOfFriends);
+console.log(bigNameOfFriends); //Ans: Sob cheye boro nam = Shukhaimura. 
